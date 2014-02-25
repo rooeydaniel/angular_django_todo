fa3b76d8409156277ff3cea60a8bd84982d85bb6
Daniel Stephenson <dan.stephenson@gmail.com>
2014-02-25 16:44:25 -0700

    You can now add todos.

--- apps/public/static/js/app.js
+++ apps/public/static/js/app.js
@@ -1,3 +1,6 @@
 'use strict';
 
-var todoApp = angular.module('todoApp', ['todoApp.controllers']);
+var todoApp = angular.module('todoApp', ['ngCookies', 'todoApp.controllers'])
+    .run(function($http, $cookies) {
+        $http.defaults.headers.common['X-CSRFToken'] = $cookies['csrftoken'];
+    });
